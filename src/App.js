39aff6cdb6d9a07af10472099cf4f0b13b36cf6c
redbanny <giorgiavakov@yandex.ru>
2020-11-09 00:27:09 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import Header from './Component/Header';
+import Header from './Component/Header/Header';
 import Topproject from './Component/Toproject';
 import FAQ from './Component/FAQ'
 
